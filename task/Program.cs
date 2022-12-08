ad0d95d35a7cf09a6cb26fa5ddaf80aa72a3707c
our ame <Anna>
2022-12-08 10:07:37 +0500

    предположительное пешение

---
+++ task/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк
+// формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+
+Console.Clear();
+string[] firstArray = new string[6] {"234", "Hello", "-7", "Goodbye", "5", "-27" };
+string[] resultArray = new string[6];
+FillingTheArray(firstArray, resultArray);
+
+void FillingTheArray(string[] firstArray, string[] resultArray)
+{
+    var z = 0;
+    for(var i = 0; i < 6; i++)
+    {
+        if(firstArray[i].Length <= 3)
+        {
+            resultArray[z] = firstArray[i];
+        }
+    }
+}
